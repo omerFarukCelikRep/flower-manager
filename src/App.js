816b723d185697f8c24cacd174b48ae63ee13f6f
Tunahandmrdvn <tunahandmrdvn@gmail.com>
2022-12-23 15:09:33 +0300

    feature-6 not found page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Footer from "./components/shared/footer/Footer";
+import { NotFound } from "./components/shared/not-Found/NotFound";
 
 function App() {
   return (
-    <>
-      <Footer />
-    </>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Footer />}></Route>
+        <Route exact path="*" element={<NotFound />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
