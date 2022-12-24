2d984cd0ec6c5d5ba3d481e0f54a3b3e21e4aea7
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-12-24 20:40:49 +0300

    env dosyası yeniden düzenlendi

--- src/env/env.js
+++
@@ -1,7 +0,0 @@
-const api = 'http://localhost:4732/';
-
-const Env ={
-    api
-};
-
-export default Env;
--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -1,24 +1,20 @@
-import Env from "../env/env";
 
 const axios = require("axios");
+const api = process.env.API_URL;
 
-const getAsync = (endpoint) => {
-  return axios.get(`${Env.api}${endpoint}`);
-};
+const getAsync = (endpoint) => axios.get(`${api}${endpoint}`);
 
 const postAsync = (endpoint, data) => {
   data.createdDate = new Date();
-  return axios.post(`${Env.api}${endpoint}`, data);
+  return axios.post(`${api}${endpoint}`, data);
 };
 
 const putAsync = (endpoint, data) => {
   data.updatedDate = new Date();
-  return axios.put(`${Env.api}${endpoint}`, data);
+  return axios.put(`${api}${endpoint}`, data);
 };
 
-const deleteAsync = (endpoint, id) => {
-  return axios.delete(`${Env.api}${endpoint}/${id}`);
-};
+const deleteAsync = (endpoint, id) => axios.delete(`${api}${endpoint}/${id}`);
 
 const ApiService = { getAsync, postAsync, putAsync, deleteAsync };
 
