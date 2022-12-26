b4cd26bae7eab40a8b7e06c087880bc7d76ec446
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-12-26 23:46:14 +0300

    her input için onchange event eklendi

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const EMAIL_REGEX = /^\S+@\S+\.\S+$/;
 const PASSWORD_REGEX =
@@ -23,6 +23,16 @@ const Register = (/*{register}*/) => {
     password: false,
     confirmedPassword: false,
   });
+  // const [registerUserFocus, setRegisterUserFocus] = useState({
+  //   firstName: false,
+  //   lastName: false,
+  //   email: false,
+  //   username: false,
+  //   dateOfBirth: false,
+  //   password: false,
+  //   confirmedPassword: false,
+  // });
+
   //register(registerUser);
 
   return (
@@ -30,37 +40,93 @@ const Register = (/*{register}*/) => {
       <form className="register-form">
         <div className="form-group">
           <label htmlFor="firstName"></label>
-          <input type="text" />
+          <input
+            type="text"
+            onChange={(event) =>
+              setRegisterUser((prevUser) => ({
+                ...prevUser,
+                firstName: event.target.value,
+              }))
+            }
+          />
           <span></span>
         </div>
         <div className="form-group">
           <label htmlFor="lastName"></label>
-          <input type="text" />
+          <input
+            type="text"
+            onChange={(event) =>
+              setRegisterUser((prevUser) => ({
+                ...prevUser,
+                lastName: event.target.value,
+              }))
+            }
+          />
           <span></span>
         </div>
         <div className="form-group">
           <label htmlFor="email"></label>
-          <input type="email" />
+          <input
+            type="email"
+            onChange={(event) =>
+              setRegisterUser((prevUser) => ({
+                ...prevUser,
+                email: event.target.value,
+              }))
+            }
+          />
           <span></span>
         </div>
         <div className="form-group">
           <label htmlFor="username"></label>
-          <input type="text" />
+          <input
+            type="text"
+            onChange={(event) =>
+              setRegisterUser((prevUser) => ({
+                ...prevUser,
+                username: event.target.value,
+              }))
+            }
+          />
           <span></span>
         </div>
         <div className="form-group">
           <label htmlFor="dateOfBirth"></label>
-          <input type="date" />
+          <input
+            type="date"
+            onChange={(event) =>
+              setRegisterUser((prevUser) => ({
+                ...prevUser,
+                dateOfBirth: event.target.value,
+              }))
+            }
+          />
           <span></span>
         </div>
         <div className="form-group">
           <label htmlFor="password"></label>
-          <input type="password" />
+          <input
+            type="password"
+            onChange={(event) =>
+              setRegisterUser((prevUser) => ({
+                ...prevUser,
+                password: event.target.value,
+              }))
+            }
+          />
           <span></span>
         </div>
         <div className="form-group">
           <label htmlFor="checkPassword"></label>
-          <input type="password" />
+          <input
+            type="password"
+            onChange={(event) =>
+              setRegisterUser((prevUser) => ({
+                ...prevUser,
+                confirmedPassword: event.target.value,
+              }))
+            }
+          />
           <span></span>
         </div>
       </form>
