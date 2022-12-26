e59f65d910829cb18d4db78de9dbc6306fa7371b
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-12-26 23:05:34 +0300

    NotFound dosya tipi jsx olarak değiştirildi

---
+++ src/components/shared/not-Found/NotFound.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="not-found">
+      <h1>404</h1>
+      <h5>Not Found</h5>
+      <p>The resource requested could not be found on this server</p>
+      <button>return to home page</button>
+    </div>
+  );
+};
+
+export default NotFound;
