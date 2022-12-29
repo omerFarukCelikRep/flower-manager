ec2f6f84bc52ca62d01c02f54723b20bbe7a8ee7
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-12-30 01:57:19 +0300

    addAsync fonksiyonu eklendi

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,15 +1,31 @@
 import ApiService from "./ApiService";
 
-const { getAsync } = ApiService;
+const { getAsync, postAsync, getLastIdAsync } = ApiService;
+const endpoint = "/users";
 
-const getByEmail = async email => {
-    const res = await getAsync("users");
+const getByEmailAsync = async (email) => {
+  const res = await getAsync(endpoint);
 
-    return res.data.find(user => user.email === email); 
-}
+  return res.data.find((user) => user.email === email);
+};
+
+const addAsync = async (user) => {
+  let lastId = await getLastIdAsync(endpoint);
+
+  user.id = lastId + 1;
+  user.createdDate = new Date();
+
+  const res = await postAsync(endpoint, user);
+  if (res.status > 199 && res.status < 300) {
+    return res.data;
+  }
+
+  return {}; //TODO: Dönüş verisini düzelt
+};
 
 const UserService = {
-    getByEmail
-}
+  getByEmailAsync,
+  addAsync,
+};
 
 export default UserService;
