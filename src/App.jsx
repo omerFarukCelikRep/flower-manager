6c8cf71a1c20d795647846c6762d4e74c026c6f8
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-01-02 02:20:46 +0300

    useNavigate ve useLocation eklendi

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import { useLocation, useNavigate } from "react-router-dom";
 import "./App.css";
 import Routers from "./components/routers/Routers";
+import { history } from "./helpers/HistoryHelper";
 
-
-function App(){
+function App() {
+  history.navigate = useNavigate();
+  history.location = useLocation();
   return (
     <>
-      <Routers />    
+      <Routers />
     </>
   );
 }
