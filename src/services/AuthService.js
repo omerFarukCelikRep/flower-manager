ab781c755be09233d1331a99aaa762881601ed07
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-01-01 16:52:48 +0300

    login fonksiyonu eklendi

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -5,18 +5,20 @@ const { getByEmailAsync, addAsync } = UserService;
 const register = async (user) => {
   //Email kontrol
   const foundUser = await getByEmailAsync(user.email);
-  if(foundUser){
+  if (foundUser) {
     return false; //TODO: Dönüş tipini düzelt
   }
-  
+
   // Kullanıcı ekle
   const addedUser = await addAsync(user);
   return addedUser ? true : false;
-  
 };
+
+const login = async (email, password) => {};
 
 const AuthService = {
   register,
+  login
 };
 
 export default AuthService;
