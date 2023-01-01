86cf73bf0b71a329fb1d4d670ed5ec0de394eaf8
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-01-02 02:33:52 +0300

    result değişkeni ve login fonksiyonu eklendi

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -3,7 +3,6 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
   timeout: 1000,
-  // headers: {'X-Custom-Header': 'foobar'}
 });
 
 const getAsync = (endpoint) => instance.get(endpoint);
@@ -24,7 +23,7 @@ const ApiService = {
   postAsync,
   putAsync,
   deleteAsync,
-  getLastIdAsync,
+  getLastIdAsync
 };
 
 export default ApiService;
--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,24 +1,55 @@
+import { useContext } from "react";
+import AuthContext from "../context/AuthProvider";
 import UserService from "./UserService";
 
 const { getByEmailAsync, addAsync } = UserService;
-
-const register = async (user) => {
-  //Email kontrol
-  const foundUser = await getByEmailAsync(user.email);
-  if (foundUser) {
-    return false; //TODO: Dönüş tipini düzelt
-  }
-
-  // Kullanıcı ekle
-  const addedUser = await addAsync(user);
-  return addedUser ? true : false;
+const result = {
+  isSuccess: false,
+  message: "",
+  data: null,
 };
 
-const login = async (email, password) => {};
+const register = async (user) => {
+  const foundUser = await getByEmailAsync(user.email);
+  if (foundUser) {
+    result.message = "There is already a user, registered with the email! ";
+    return result;
+  }
+
+  delete user.confirmedPassword;
+  const addedUser = await addAsync(user);
+  if (addedUser) {
+    result.data = addedUser;
+    result.message = "Registered Successfully";
+    result.isSuccess = true;
+  } else {
+    result.message = "Invalid Request!";
+  }
+
+  return result;
+};
+
+const login = async (email, password) => {
+  result.message = "Email or password wrong!";
+  const user = await getByEmailAsync(email);
+  if (!user) {
+    return result;
+  }
+
+  if (user.password !== password) {
+    return result;
+  }
+
+  result.isSuccess = true;
+  result.message = "Login Success";
+  result.data = user;
+
+  return result;
+};
 
 const AuthService = {
   register,
-  login
+  login,
 };
 
 export default AuthService;
