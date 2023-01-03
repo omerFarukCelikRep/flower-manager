6e8bdaebca8112026d327946a2483e7fb7960e03
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-01-04 01:14:51 +0300

    scss import eklendi

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useLocation, useNavigate } from "react-router-dom";
-import "./App.css";
+import "./App.scss";
 import Routers from "./components/routers/Routers";
 import { history } from "./helpers/HistoryHelper";
 
