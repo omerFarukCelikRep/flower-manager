aed79fc0ecd91226d09e9d931926b8d29362ed84
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-01-04 01:16:44 +0300

    Header, navbar ve giriş butonlarının olduğu komponentlere göre yeniden düzenlendi

--- src/components/shared/header/Header.jsx
+++ src/components/shared/header/Header.jsx
@@ -1,47 +1,16 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { useAuthContext } from "../../../context/AuthProvider";
-import { history } from "../../../helpers/HistoryHelper";
+import Auth from "../Auth/Auth";
+import Navbar from "../navbar/Navbar";
 
 const Header = () => {
-  const { auth, setAuth } = useAuthContext();
-
-  const logout = () => {
-    setAuth();
-    history.navigate("/");
-  };
   return (
-    <div className="navbar">
-      <div className="container">
-        <div className="logo">
-          <img src="#" alt="flower-manager-logo" />
-        </div>
-        <div className="navbar-texts">
-          <ul>
-            <Link to="/">Home</Link>
-            <Link to="/About">About</Link>
-            <Link to="/Products">Products</Link>
-            <Link to="/Contact">Contact</Link>
-          </ul>
-        </div>
-        <div className="navbar-button">
-          {!auth ? (
-            <>
-              <Link to="/register">
-                <button>Sign In</button>
-              </Link>
-              <Link to="/login">
-                <button>Log In</button>
-              </Link>
-            </>
-          ) : (
-            <>
-              <button onClick={logout}>Logout</button>
-            </>
-          )}
-        </div>
+    <header className="app-header">
+      <div className="app-logo">
+        <img src="#" alt="flower-manager-logo" />
       </div>
-    </div>
+      <Navbar />
+      <Auth />
+    </header>
   );
 };
 
