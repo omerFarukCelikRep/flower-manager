d591b688111efd1afac85e2819841a1ac513c815
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-01-05 00:55:48 +0300

    useAuthContext kullanıldı

--- src/components/routers/PrivateRoute.jsx
+++ src/components/routers/PrivateRoute.jsx
@@ -1,10 +1,10 @@
-import React, { useContext } from "react";
+import React from "react";
 import { Navigate } from "react-router-dom";
-import AuthContext from "../../context/AuthProvider";
+import { useAuthContext } from "../../context/AuthProvider";
 import { history } from "../../helpers/HistoryHelper";
 
 const PrivateRoute = ({ children }) => {
-  const { auth } = useContext(AuthContext);
+  const { auth } = useAuthContext();
   if (!auth) {
     return <Navigate to="/login" state={{ from: history.location }} />;
   }
