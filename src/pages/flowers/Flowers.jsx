b91ea066c13f0c72dd4fe25af82188ce1853e497
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-01-06 01:09:48 +0300

    Products komponenti, Flowers olarak yeniden isimlendirildi

---
+++ src/pages/flowers/Flowers.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import FlowerList from "../../components/app/flowers/list/FlowerList";
+import '../flowers/flowers.scss';
+
+const Flowers = () => {
+  return (
+    <>
+      <FlowerList />
+    </>
+  );
+};
+export default Flowers;
