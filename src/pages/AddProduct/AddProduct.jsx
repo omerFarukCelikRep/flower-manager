54920bd81874e0613cf82431fcadd7b9222353ad
Tunahandmrdvn <tunahandmrdvn@gmail.com>
2023-01-08 00:41:00 +0300

    AddProduct page eklendi

---
+++ src/pages/AddProduct/AddProduct.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import  ProductForm  from "../../ProductForm/ProductForm";
+
+export const AddProduct = () => {
+    
+  return (
+    <>
+    <ProductForm/>
+    </>
+  )
+}
