2f0922c25a8e70e9298860a9b33526349e76e3c7
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-01-08 18:35:43 +0300

    route düzenlendi

--- src/components/routers/Routers.jsx
+++ src/components/routers/Routers.jsx
@@ -5,13 +5,14 @@ import NotFound from "../../pages/not-Found/NotFound";
 import Home from "../../pages/home/Home";
 import About from "../../pages/about/About";
 import Contact from "../../pages/contact/Contact";
-import Layout from "../../pages/layOut/LayOut";
+import Layout from "../../pages/layout/Layout";
 import Flowers from "../../pages/flowers/Flowers";
 import Auth from "../../pages/auth/Auth";
 import { AddProduct } from "../../pages/AddProduct/AddProduct";
 // import ProductDetails from '../../list/ProductDetails/ProductDetails';
 // import ProductDetails from "../../flowers/ProductDetails/ProductDetails";
 import ProductDetails from "../app/flowers/ProductDetails/ProductDetails";
+import Users from "../../pages/users/Users";
 
 const Routers = () => {
   return (
@@ -23,10 +24,12 @@ const Routers = () => {
           <Route path="/flowers" element={<Flowers />} />
           <Route path="/flowers/:id" component={ProductDetails} />
           <Route path="/Contact" element={<Contact />} />
-          <Route path="/AddProduct" element={<AddProduct />} />
-          <Route path="/register" element={<Auth />} />
-          <Route path="/auth" element={<Auth />} />
-          <Route path="/login" element={<Auth />} />
+          <Route path="/AddProduct"  element={<AddProduct />} />
+          <Route path="users/*" element={<Users />} />
+          <Route path="/" element={<Auth />}>
+            <Route path="/register" element={<Auth />} />
+            <Route path="/login" element={<Auth />} />
+          </Route>
           <Route path="*" element={<NotFound />}></Route>
         </Route>
       </Routes>
