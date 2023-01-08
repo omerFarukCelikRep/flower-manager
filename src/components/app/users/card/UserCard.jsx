76d71402ce027cc6e4ef3c47ce065c7f31eef326
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-01-08 23:09:05 +0300

    User card html elementleri yeniden düzenlendi

--- src/components/app/users/card/UserCard.jsx
+++ src/components/app/users/card/UserCard.jsx
@@ -1,24 +1,46 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { Link } from "react-router-dom";
 import BlankPicture from "../../../../img/blank-profile-photo.jpeg";
+import { faFacebook, faTwitter, faYoutube } from "@fortawesome/free-brands-svg-icons";
 
 const UserCard = ({ user }) => {
   return (
     <>
-      <div className="flower-card">
-        <div className="face face1">
-          <div className="content">
-            <img src={!user.image && BlankPicture} alt={`${user.firstName}-${user.lastName}`} />
-            <h3>{`${user.firstName}-${user.lastName}`}</h3>
+      <div className="user-card">
+        <div className="profile-image">
+          <img
+            src={!user.image && BlankPicture}
+            alt={`${user.firstName}-${user.lastName}`}
+          />
+        </div>
+        <div className="user-card-content">
+          <h3>{`${user.firstName} ${user.lastName}`}</h3>
+          <p>{user.username}</p>
+          <p>{user.email}</p>
+          <p>{user.dateOfBirth}</p>
+          <div className="social">
+            <Link to={`/users/${user.id}`}>
+              <i>
+                <FontAwesomeIcon icon={faFacebook} />
+              </i>
+            </Link>
+            <Link to={`/users/${user.id}`}>
+              <i>
+                <FontAwesomeIcon icon={faTwitter} />
+              </i>
+            </Link>
+            <Link to={`/users/${user.id}`}>
+              <i>
+                <FontAwesomeIcon icon={faYoutube} />
+              </i>
+            </Link>
           </div>
         </div>
-        <div className="face face2">
-          <div className="content">
-            <p>{user.username}</p>
-            <p>{user.email}</p>
-            <p>{user.dateOfBirth}</p>
-            <Link to={`/users/${user.id}`}>User Details</Link>
-          </div>
+        <div className="card-footer">
+          <Link to={`/users/${user.id}`} className="route-link">
+            User Details
+          </Link>
         </div>
       </div>
     </>
