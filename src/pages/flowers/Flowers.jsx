d834e6284e5a00b6ba47278f4f30fbbd0ef50427
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-01-10 01:15:40 +0300

    Flower list container içindeki header için css eklendi

--- src/pages/flowers/Flowers.jsx
+++ src/pages/flowers/Flowers.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "../flowers/flowers.scss";
 import FlowerList from "../../components/app/flowers/list/FlowerList";
 import FlowerDetails from "../../components/app/flowers/details/FlowerDetails";
@@ -8,11 +8,6 @@ import FlowerCreate from "../../components/app/flowers/create/FlowerCreate";
 const Flowers = () => {
   return (
     <>
-      <p className="create-product">
-        <Link to="create">
-          <span>Create Product</span>
-        </Link>
-      </p>
       <Routes>
         <Route index path="/" element={<FlowerList />} />
         <Route path=":id" element={<FlowerDetails />} />
