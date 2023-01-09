528c56264c12b5238ec08bc1200136f08d9775e5
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-01-09 23:01:59 +0300

    Flower sayfa routeları düzeltildi

--- src/pages/flowers/Flowers.jsx
+++ src/pages/flowers/Flowers.jsx
@@ -1,21 +1,22 @@
-
-
 import React from "react";
-import { Route, Routes } from "react-router-dom";
-import ProductDetails from "../../components/app/flowers/ProductDetails/ProductDetails";
-import Flower from "./Flower";
+import { Link, Route, Routes } from "react-router-dom";
 import "../flowers/flowers.scss";
+import FlowerList from "../../components/app/flowers/list/FlowerList";
+import FlowerDetails from "../../components/app/flowers/details/FlowerDetails";
+import FlowerCreate from "../../components/app/flowers/create/FlowerCreate";
 
 const Flowers = () => {
-
-
-
   return (
     <>
-   
+      <p className="create-product">
+        <Link to="create">
+          <span>Create Product</span>
+        </Link>
+      </p>
       <Routes>
-        <Route index path="/" element={<Flower />} />
-        <Route path=":id" element={<ProductDetails />} />
+        <Route index path="/" element={<FlowerList />} />
+        <Route path=":id" element={<FlowerDetails />} />
+        <Route path="create" element={<FlowerCreate />} />
       </Routes>
     </>
   );
