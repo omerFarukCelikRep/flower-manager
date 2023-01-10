f79929a65a829f8afa65afc0cbb957edaf81aec9
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-01-11 00:50:42 +0300

    Private Routh komponenti RequireAuth olarak isimlendirildi

--- src/components/routers/RequireAuth.jsx
+++ src/components/routers/RequireAuth.jsx
@@ -3,13 +3,13 @@ import { Navigate } from "react-router-dom";
 import { useAuthContext } from "../../context/AuthProvider";
 import { history } from "../../helpers/HistoryHelper";
 
-const PrivateRoute = ({ children }) => {
+const RequireAuth = ({ children }) => {
   const { auth } = useAuthContext();
   if (!auth) {
-    return <Navigate to="/login" state={{ from: history.location }} />;
+    return <Navigate to="/login" state={{ from: history.location }} replace />;
   }
 
   return children;
 };
 
-export default PrivateRoute;
+export default RequireAuth;
