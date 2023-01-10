9454d78f10b9cef4360f3683857a8aaf7f4aebf4
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-01-11 01:09:55 +0300

    Flower Create elementi için Require Auth eklendi

--- src/pages/flowers/Flowers.jsx
+++ src/pages/flowers/Flowers.jsx
@@ -4,6 +4,7 @@ import "../flowers/flowers.scss";
 import FlowerList from "../../components/app/flowers/list/FlowerList";
 import FlowerDetails from "../../components/app/flowers/details/FlowerDetails";
 import FlowerCreate from "../../components/app/flowers/create/FlowerCreate";
+import RequireAuth from "../../components/routers/RequireAuth";
 
 const Flowers = () => {
   return (
@@ -11,7 +12,14 @@ const Flowers = () => {
       <Routes>
         <Route index path="/" element={<FlowerList />} />
         <Route path=":id" element={<FlowerDetails />} />
-        <Route path="create" element={<FlowerCreate />} />
+        <Route
+          path="create"
+          element={
+            <RequireAuth>
+              <FlowerCreate />
+            </RequireAuth>
+          }
+        />
       </Routes>
     </>
   );
