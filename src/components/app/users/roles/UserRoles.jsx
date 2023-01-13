08de2b72dfd0bbaad21a2e0fe0d9047aab689f03
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-01-14 02:15:52 +0300

    UserRoles komponenti rolleri yanında checkbox ile listeleyecek şekilde düzenlendi

--- src/components/app/users/roles/UserRoles.jsx
+++ src/components/app/users/roles/UserRoles.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import { roles } from "../../../../context/Roles";
 
 const UserRoles = () => {
   return (
-    <div>UserRoles</div>
-  )
-}
+    <>
+      <div className="user-roles-container">
+        {roles.map((role) => (
+          <div className="check-group">
+            <input type="checkbox" />
+            <span>{role}</span>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default UserRoles;
