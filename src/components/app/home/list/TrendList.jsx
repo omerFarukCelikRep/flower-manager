c28ebbd815a33c735de4bee2a80eda0b0109692b
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-01-14 01:31:12 +0300

    Flower ürünlerin rastgele ana ekranda listelenmesini sağlayan komponent eklendi

---
+++ src/components/app/home/list/TrendList.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import ArrayHelper from "../../../../helpers/ArrayHelper";
+import FlowerService from "../../../../services/FlowerService";
+
+const TrendList = () => {
+  const [flowers, setFlowers] = useState([]);
+
+  useEffect(() => {
+    const getAllFlowers = async () => {
+      const result = await FlowerService.getAllAsync();
+      if (!result.isSuccess) {
+        return;
+      }
+
+      ArrayHelper.shuffleArray(result.data);
+
+      setFlowers(result.data.slice(0, 10));
+    };
+
+    getAllFlowers();
+  }, []);
+
+  return (
+    <>
+      <div className="top-sales box">
+        <div className="title">Trend Products</div>
+        <ul className="top-sales-details">
+          {flowers.map((flower) => (
+            <li>
+              <Link to={`/flowers/${flower.id}`}>
+                <img src={flower.image} alt={flower.name} />
+                <span className="product">{flower.name}</span>
+              </Link>
+              <span className="price">{`${flower.price} TL`}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default TrendList;
