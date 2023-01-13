a12874a6e6dbbf06efda6d5445a550c898643b25
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-01-14 01:36:00 +0300

    Ana sayfada el ile yazılan tablo verileri TrendList komponenti ile değiştirildi

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import TrendList from "../../components/app/home/list/TrendList";
 import Overview from "../../components/app/home/overview/Overview";
 import "./home.scss";
 
@@ -9,67 +10,7 @@ const Home = () => {
         <div className="home-content">
           <Overview />
           <div className="sales-boxes">
-            <div className="top-sales box">
-              <div className="title">Top Seling Product</div>
-              <ul className="top-sales-details">
-                <li>
-                  <a href="#">
-                    <img src="images/sunglasses.jpg" alt="" />
-                    <span className="product">Vuitton Sunglasses</span>
-                  </a>
-                  <span className="price">$1107</span>
-                </li>
-                <li>
-                  <a href="#">
-                    <img src="images/jeans.jpg" alt="" />
-                    <span className="product">Hourglass Jeans </span>
-                  </a>
-                  <span className="price">$1567</span>
-                </li>
-                <li>
-                  <a href="#">
-                    <img src="images/nike.jpg" alt="" />
-                    <span className="product">Nike Sport Shoe</span>
-                  </a>
-                  <span className="price">$1234</span>
-                </li>
-                <li>
-                  <a href="#">
-                    <img src="images/scarves.jpg" alt="" />
-                    <span className="product">Hermes Silk Scarves.</span>
-                  </a>
-                  <span className="price">$2312</span>
-                </li>
-                <li>
-                  <a href="#">
-                    <img src="images/blueBag.jpg" alt="" />
-                    <span className="product">Succi Ladies Bag</span>
-                  </a>
-                  <span className="price">$1456</span>
-                </li>
-                <li>
-                  <a href="#">
-                    <img src="images/bag.jpg" alt="" />
-                    <span className="product">Gucci Womens's Bags</span>
-                  </a>
-                  <span className="price">$2345</span>
-                </li>
-                <li>
-                  <a href="#">
-                    <img src="images/addidas.jpg" alt="" />
-                    <span className="product">Addidas Running Shoe</span>
-                  </a>
-                  <span className="price">$2345</span>
-                </li>
-                <li>
-                  <a href="#">
-                    <img src="images/shirt.jpg" alt="" />
-                    <span className="product">Bilack Wear's Shirt</span>
-                  </a>
-                  <span className="price">$1245</span>
-                </li>
-              </ul>
-            </div>
+            <TrendList />
           </div>
         </div>
       </div>
