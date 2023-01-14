ee6ba0c65764eb5552ef8b96160202c76167fd73
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-01-15 02:30:42 +0300

    Kullanıcı rollerini görüntülediğimiz komponentte daha önce atanan rollerin seçili olarak gelmesi sağlandı

--- src/components/app/users/roles/UserRoles.jsx
+++ src/components/app/users/roles/UserRoles.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { roles } from "../../../../context/Roles";
 
-const UserRoles = () => {
+const UserRoles = ({ userRoles, handleChecboxChange }) => {
   return (
     <>
       <div className="user-roles-container">
         {roles.map((role, index) => (
           <div className="check-group" key={index}>
-            <input type="checkbox" />
+            <input
+              type="checkbox"
+              value={role}
+              defaultChecked={userRoles?.some((userRole) => userRole === role)}
+              // checked={userRoles?.some((userRole) => userRole === role)}
+              onChange={handleChecboxChange}
+            />
             <span>{role}</span>
           </div>
         ))}
