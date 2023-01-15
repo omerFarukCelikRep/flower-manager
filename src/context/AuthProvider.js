73566e98217193bebf76fabe78dfbe9e79e07dbc
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-01-15 15:53:31 +0300

    AuthProvider içerisine admin kontrolü için state eklendi

--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -1,12 +1,22 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [auth, setAuth] = useState();
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  useEffect(() => {
+    if (!auth) {
+      setIsAdmin(false);
+      return;
+    }
+
+    setIsAdmin(auth.roles.some((role) => role === "Admin"));
+  }, [auth]);
 
   return (
-    <AuthContext.Provider value={{ auth, setAuth }}>
+    <AuthContext.Provider value={{ auth, setAuth, isAdmin }}>
       {children}
     </AuthContext.Provider>
   );
