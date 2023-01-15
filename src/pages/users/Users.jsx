d57690cc88681a9e77b9165a5227234660bcc1f7
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-01-15 15:54:47 +0300

    User sayfalarının sadece admin tarafından görüntülenebilmesi için Require role içerisine alındı

--- src/pages/users/Users.jsx
+++ src/pages/users/Users.jsx
@@ -2,14 +2,30 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import UserDetails from "../../components/app/users/details/UserDetails";
 import UserList from "../../components/app/users/list/UserList";
+import RequireRole from "../../components/routers/RequireRole";
 import "./users.scss";
 
 const Users = () => {
   return (
     <>
       <Routes>
-        <Route index path="/" element={<UserList />} />
-        <Route path=":id" element={<UserDetails />} />
+        <Route
+          index
+          path="/"
+          element={
+            <RequireRole role={"Admin"}>
+              <UserList />
+            </RequireRole>
+          }
+        />
+        <Route
+          path=":id"
+          element={
+            <RequireRole role={"Admin"}>
+              <UserDetails />
+            </RequireRole>
+          }
+        />
       </Routes>
     </>
   );
