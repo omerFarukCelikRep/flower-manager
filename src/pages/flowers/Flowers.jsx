a0bfddc936fc9986cd9c849ba329d344a9b832a9
Tunahandmrdvn <tunahandmrdvn@gmail.com>
2023-01-15 23:17:27 +0300

    kullanıcı kendi ürünlerini görme eklendi

--- src/pages/flowers/Flowers.jsx
+++ src/pages/flowers/Flowers.jsx
@@ -4,13 +4,22 @@ import FlowerList from "../../components/app/flowers/list/FlowerList";
 import FlowerDetails from "../../components/app/flowers/details/FlowerDetails";
 import FlowerCreate from "../../components/app/flowers/create/FlowerCreate";
 import RequireAuth from "../../components/routers/RequireAuth";
+import MyFlower from "../myFlower/MyFlower";
+import { useAuthContext } from "../../context/AuthProvider";
 import "../flowers/flowers.scss";
 
 const Flowers = () => {
+  const { auth } = useAuthContext();
   return (
+    
     <>
       <Routes>
-        <Route index path="/" element={<FlowerList />} />
+      <Route 
+          path="/" 
+          element={
+            auth ? <MyFlower /> : <FlowerList />
+          }
+        />
         <Route path=":id" element={<FlowerDetails />} />
         <Route
           path="create"
@@ -21,6 +30,7 @@ const Flowers = () => {
           }
         />
       </Routes>
+      
     </>
   );
 };
