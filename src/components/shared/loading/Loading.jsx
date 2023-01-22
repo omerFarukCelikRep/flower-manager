de3f3279d297fa07c88cbd991f294d92b3e90118
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-01-23 01:45:16 +0300

    Loading komponenti eklendi

---
+++ src/components/shared/loading/Loading.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <>
+    <div className="loading-container">
+        <div className="loader"><span></span></div>
+        <div className="loader"><span></span></div>
+        <div className="loader"><i></i></div>
+        <div className="loader"><i></i></div>
+    </div>
+    </>
+  )
+}
+
+export default Loading;
