7e94d133fd7b61d3771a39f685b38afd7f5a9abc
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-01-23 01:44:32 +0300

    UserDetails'a loading eklendi

--- src/components/app/users/details/UserDetails.jsx
+++ src/components/app/users/details/UserDetails.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import { history } from "../../../../helpers/HistoryHelper";
 import BlankPicture from "../../../../img/blank-profile-photo.jpeg";
 import UserService from "../../../../services/UserService";
+import Loading from "../../../shared/loading/Loading";
 import Modal, {
   ModalBody,
   ModalFooter,
@@ -17,15 +18,18 @@ const UserDetails = () => {
   const [user, setUser] = useState({});
   const [showModal, setShowModal] = useState(false);
   const [addedRoles, setAddedRoles] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const getUser = async (id) => {
+      setLoading(true);
       const result = await UserService.getByIdAsync(id);
       if (!result.isSuccess) {
         history.navigate("/users");
       }
       setUser(result.data);
       setAddedRoles([...result.data.roles]);
+      setLoading(false);
     };
 
     getUser(id);
@@ -54,73 +58,77 @@ const UserDetails = () => {
 
   return (
     <>
-      <div className="user-details-container">
-        <aside className="profile-image">
-          <img
-            src={!user.image && BlankPicture}
-            alt={`${user.firstName}-${user.lastName}`}
-          />
-          <button
-            type="button"
-            className="route-link"
-            onClick={() => setShowModal(true)}
-          >
-            Roles
-          </button>
-          <Link to={`../update/${id}`} className="route-link">
-            Update
-          </Link>
-          <Link to={`../delete/${id}`} className="route-link danger">
-            Delete
-          </Link>
-          <Modal
-            show={showModal}
-            setShowModal={setShowModal}
-            /*hideCloseButton*/
-          >
-            <form onSubmit={addToRole}>
-              <ModalHeader>
-                <h2>User Roles</h2>
-              </ModalHeader>
-              <ModalBody>
-                <UserRoles
-                  userRoles={user.roles}
-                  handleChecboxChange={handleChecboxChange}
-                />
-              </ModalBody>
-              <ModalFooter>
-                <button
-                  type="button"
-                  className="route-link secondary"
-                  onClick={() => setShowModal(false)}
-                >
-                  Close
-                </button>
-                <button type="submit" className="route-link primary">
-                  Add Role
-                </button>
-              </ModalFooter>
-            </form>
-          </Modal>
-        </aside>
-        <main className="details">
-          <div className="details-title">{`${user.firstName} ${user.lastName}`}</div>
-          <div className="details-content">
-            <div className="content-group">
-              <label>Username</label>:<span>{user.username}</span>
+      {loading ? (
+        <Loading />
+      ) : (
+        <div className="user-details-container">
+          <aside className="profile-image">
+            <img
+              src={!user.image && BlankPicture}
+              alt={`${user.firstName}-${user.lastName}`}
+            />
+            <button
+              type="button"
+              className="route-link"
+              onClick={() => setShowModal(true)}
+            >
+              Roles
+            </button>
+            <Link to={`../update/${id}`} className="route-link">
+              Update
+            </Link>
+            <Link to={`../delete/${id}`} className="route-link danger">
+              Delete
+            </Link>
+            <Modal
+              show={showModal}
+              setShowModal={setShowModal}
+              /*hideCloseButton*/
+            >
+              <form onSubmit={addToRole}>
+                <ModalHeader>
+                  <h2>User Roles</h2>
+                </ModalHeader>
+                <ModalBody>
+                  <UserRoles
+                    userRoles={user.roles}
+                    handleChecboxChange={handleChecboxChange}
+                  />
+                </ModalBody>
+                <ModalFooter>
+                  <button
+                    type="button"
+                    className="route-link secondary"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Close
+                  </button>
+                  <button type="submit" className="route-link primary">
+                    Add Role
+                  </button>
+                </ModalFooter>
+              </form>
+            </Modal>
+          </aside>
+          <main className="details">
+            <div className="details-title">{`${user.firstName} ${user.lastName}`}</div>
+            <div className="details-content">
+              <div className="content-group">
+                <label>Username</label>:<span>{user.username}</span>
+              </div>
+              <div className="content-group">
+                <label>Email</label>:<span>{user.email}</span>
+              </div>
+              <div className="content-group">
+                <label>Date Of Birth</label>:<span>{user.dateOfBirth}</span>
+              </div>
+              <div className="content-group">
+                <label>Roles</label>:<span>{user.roles?.join(", ")}</span>
+              </div>
             </div>
-            <div className="content-group">
-              <label>Email</label>:<span>{user.email}</span>
-            </div>
-            <div className="content-group">
-              <label>Date Of Birth</label>:<span>{user.dateOfBirth}</span>
-            </div>
-            <div className="content-group">
-              <label>Roles</label>:<span>{user.roles?.join(", ")}</span>
-            </div>
-          </div>
-        </main>
-      </div>
+          </main>
+        </div>
+      )}
     </>
   );
 };
