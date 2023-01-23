2b21e895f0a7f40e29e792e19ca17cafb49b084f
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-01-23 23:50:30 +0300

    User Service deleteUserAsync fonksiyonu eklendi

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,6 +1,7 @@
 import ApiService from "./ApiService";
 
-const { getAsync, postAsync, getLastIdAsync, putAsync } = ApiService;
+const { getAsync, postAsync, getLastIdAsync, putAsync, deleteAsync } =
+  ApiService;
 const endpoint = "/users";
 
 const getAllAsync = async () => {
@@ -72,6 +73,7 @@ const addToRoleAsync = async (id, roles) => {
     }
 
     const user = res.data;
+    user.modifiedDate = new Date();
     user.roles = roles;
 
     const putResponse = await putAsync(`${endpoint}/${id}`, user);
@@ -94,12 +96,72 @@ const addToRoleAsync = async (id, roles) => {
   }
 };
 
+const updateAsync = async (user) => {
+  try {
+    const res = await getAsync(`${endpoint}/${user.id}`);
+    if (res.status < 200 && res.status > 299) {
+      return {
+        isSuccess: false,
+        message: res.status,
+      };
+    }
+
+    let foundUser = res.data;
+    foundUser = { ...user };
+    foundUser.modifiedDate = new Date();
+
+    const putResponse = await putAsync(
+      `${endpoint}/${foundUser.id}`,
+      foundUser
+    );
+    if (putResponse.status < 200 && putResponse.status > 299) {
+      return {
+        isSuccess: false,
+        message: putResponse.status,
+      };
+    }
+
+    return {
+      isSuccess: true,
+      data: user,
+    };
+  } catch (error) {
+    return {
+      isSuccess: false,
+      message: error.response.status,
+    };
+  }
+};
+
+const deleteUserAsync = async (id) => {
+  try {
+    const res = await deleteAsync(endpoint, id);
+    if (res.status < 200 && res.status > 299) {
+      return {
+        isSuccess: false,
+        message: res.status,
+      };
+    }
+
+    return {
+      isSuccess: true,
+    };
+  } catch (error) {
+    return {
+      isSuccess: false,
+      message: error.response.status,
+    };
+  }
+};
+
 const UserService = {
   getAllAsync,
   getByEmailAsync,
   addAsync,
   getByIdAsync,
   addToRoleAsync,
+  updateAsync,
+  deleteAsync: deleteUserAsync,
 };
 
 export default UserService;
