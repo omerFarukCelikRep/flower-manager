984e870cea8300a4a8ecff27d336d4ad7640e730
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-01-31 01:03:06 +0300

    unauthorized komponent eklendi

---
+++ src/pages/unauthorized/Unauthorized.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import "./unauthorized.scss";
+
+const Unauthorized = () => {
+  return (
+    <>
+      <div className="maincontainer">
+        <div className="bat">
+          <img
+            className="wing leftwing"
+            src="https://www.blissfullemon.com/wp-content/uploads/2018/09/bat-wing.png"
+            alt=""
+          />
+          <img
+            className="body"
+            src="https://www.blissfullemon.com/wp-content/uploads/2018/09/bat-body.png"
+            alt="bat"
+          />
+          <img
+            className="wing rightwing"
+            src="https://www.blissfullemon.com/wp-content/uploads/2018/09/bat-wing.png"
+            alt=""
+          />
+        </div>
+        <div className="bat">
+          <img
+            className="wing leftwing"
+            src="https://www.blissfullemon.com/wp-content/uploads/2018/09/bat-wing.png"
+            alt=""
+          />
+          <img
+            className="body"
+            src="https://www.blissfullemon.com/wp-content/uploads/2018/09/bat-body.png"
+            alt="bat"
+          />
+          <img
+            className="wing rightwing"
+            src="https://www.blissfullemon.com/wp-content/uploads/2018/09/bat-wing.png"
+            alt=""
+          />
+        </div>
+        <div className="bat">
+          <img
+            className="wing leftwing"
+            src="https://www.blissfullemon.com/wp-content/uploads/2018/09/bat-wing.png"
+            alt=""
+          />
+          <img
+            className="body"
+            src="https://www.blissfullemon.com/wp-content/uploads/2018/09/bat-body.png"
+            alt="bat"
+          />
+          <img
+            className="wing rightwing"
+            src="https://www.blissfullemon.com/wp-content/uploads/2018/09/bat-wing.png"
+            alt=""
+          />
+        </div>
+        <img
+          className="foregroundimg"
+          src="https://www.blissfullemon.com/wp-content/uploads/2018/09/HauntedHouseForeground.png"
+          alt="haunted house"
+        />
+      </div>
+      <h2 className="errorcode">ERROR 403</h2>
+      <div className="errortext">This area is forbidden. Turn back now!</div>
+    </>
+  );
+};
+
+export default Unauthorized;
